bca5e7678bc00bbc452f449d2c9caa023788b122
Yash <51404592+Yashmax15@users.noreply.github.com>
2019-06-23 18:21:28 +0530

    added character boundaries acc to map 01

--- HER.cpp
+++ HER.cpp
@@ -18,7 +18,7 @@
 #include<conio.h>
 #include<stdio.h>
 #include<graphics.h>
-#include<HERO.h>
+#include<LEVEL_01.h>
 
 void main()
 {clrscr();
@@ -26,8 +26,5 @@ int gd=DETECT,gm;
 //initgraph(&gd,&gm,"C:\\TURBOC3\\BGI"); //main call
 initgraph(&gd,&gm,"C:\\TC\\BGI");//for @Yash 's PC only, comment it out for your own PC and use the upar waala statement
 setbkcolor(WHITE);
-make_room();
-int x=200,y=200;
-hero_f(x,y);
-move_hero(x,y,100,getmaxx(),150,getmaxy(),100,getmaxx(),150,getmaxy());
+lvl_01();
 getch();}
