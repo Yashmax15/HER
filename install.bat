b8f33f0e39757ec82c0c6e2f6026960b68dc1468
Snehit Sah <cdbusinesssnehi@gmail.com>
2019-06-27 21:16:45 +0530

    Create install.bat
    Automatically copies the files to required folder, considering that the HER folder is placed inside Turbo C++ directory.
    
    initgraph() call still needs to be updated manually.

---
+++ install.bat
@@ -0,0 +1,9 @@
+@echo HER Automatic Installer
+@echo Auotmatic install
+@echo Copying header files to INCLUDE
+copy *.H ..\INCLUDE\
+@echo Header files copied to INCLUDE folder
+@echo Copying PLAYER.dat to BIN folder
+copy PLAYER.dat ..\BIN\
+@echo PLAYER.dat copied to BIN folder
+pause
